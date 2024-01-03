d92cd2d9a07dd933d317f7000f0493208bb0430a
afifa <2023afifa@gmail.com>
2024-01-03 18:32:42 +0600

    Banner section updated

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-slate-50">
-                <div className="hero-content flex-col lg:flex-row gap-10">
-                    <img src="https://i.ibb.co/ggjz14S/banner.jpg" className="max-w-sm rounded-lg shadow-lg" />
-                    <div className="">
+            <div className="hero bg-gray-100 mb-10">
+                <div className="hero-content flex-col lg:flex-row gap-10 max-w-full">
+                    <img src="https://i.ibb.co/CP7PpYs/banner.jpg" className="max-w-3xl rounded-lg shadow-lg" />
+                    <div>
                         <h1 className="text-4xl text-rose-800 font-bold">Nourishing Communities,<br /> One Heartfelt Share at a Time.</h1>
-                        <p className="py-6 text-slate-500 text-sm">Welcome to ShareWithHeart, where kindness meets sustainability. Our platform is a community of caring individuals dedicated to reducing food waste and supporting those in need. Together, we are on a mission to make the world a better place by sharing surplus food with love and compassion. Join our community, share your heart, and be part of a movement that nourishes both the body and soul.</p>
+                        <p className="py-6 text-slate-500 italic text-sm">Welcome to ShareWithHeart, where kindness meets sustainability. Our platform is a community of caring individuals dedicated to reducing food waste and supporting those in need. Together, we are on a mission to make the world a better place by sharing surplus food with love and compassion. Join our community, share your heart, and be part of a movement that nourishes both the body and soul.</p>
                     </div>
                 </div>
             </div>
