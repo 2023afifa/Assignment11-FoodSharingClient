174715773fcbef68a91769955188d3e1315feff7
afifa <2023afifa@gmail.com>
2024-02-11 21:32:36 +0600

    Login page updated

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -26,12 +26,12 @@ const Login = () => {
                 axios.post("http://localhost:5000/jwt", user, {
                     withCredentials: true
                 })
-                .then(res => {
-                    console.log(res.data);
-                    if (res.data.success) {
-                        navigate(location?.state ? location?.state : "/");
-                    }
-                })
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.success) {
+                            navigate(location?.state ? location?.state : "/");
+                        }
+                    })
             })
             .catch(error => {
                 console.error(error);
@@ -51,12 +51,12 @@ const Login = () => {
 
     const handleGithubLogIn = () => {
         logInUserGithub()
-        .then(result => {
-            console.log(result.user);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     return (
@@ -65,31 +65,31 @@ const Login = () => {
                 <title>ShareWithHeart | Log In</title>
             </Helmet>
             <Navbar></Navbar>
-            <div className="card max-w-xl shadow-xl bg-rose-100 mt-20 mx-auto">
-                <h2 className="text-center text-2xl text-rose-700 font-semibold pt-10">Log In</h2>
+            <div className="card max-w-xl shadow-xl bg-amber-300 my-20 mx-auto rounded-sm">
+                <h2 className="text-center text-2xl text-amber-700 font-semibold pt-10">Log In</h2>
+                <p className=" text-center">If you do not have any account <Link to="/signup"><span className="text-amber-700 font-semibold">Sign up</span></Link> here</p>
+                <p className="text-center">Or, You Can Login with <a onClick={handleGoogleLogIn} className="text-amber-700 font-semibold">Google</a> / <a onClick={handleGithubLogIn} className="text-amber-700 font-semibold">Github</a></p>
                 <form onSubmit={handleLogin} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="email" className="input input-bordered rounded-sm" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" name="password" placeholder="password" className="input input-bordered rounded-sm" required />
                     </div>
                     {
                         errorMessage && <p className="text-center text-red-500">{errorMessage}</p>
                     }
-                    <div className="form-control mt-6">
-                        <button className="btn bg-rose-200 text-rose-700">Login</button>
+                    <div className="mt-6 mx-auto">
+                        <button className="btn normal-case text-lg bg-amber-700 text-white hover:bg-amber-600 rounded-sm border-none">Login</button>
                     </div>
                 </form>
-                <p className="text-center pb-8">Login with <a onClick={handleGoogleLogIn} className="text-rose-700 font-semibold">Google</a> / <a onClick={handleGithubLogIn} className="text-rose-700 font-semibold">Github</a></p>
             </div>
-            <p className="my-10 text-center">If you do not have any account <Link to="/signup"><span className="text-rose-700 font-semibold">Sign up</span></Link> here</p>
             <Footer></Footer>
         </div>
     );
