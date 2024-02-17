34479f024832b567cb9da559f2cce2ba10c4f7c9
afifa <2023afifa@gmail.com>
2024-02-17 22:23:36 +0600

    Details page updated

--- src/Details/Details.jsx
+++ src/Details/Details.jsx
@@ -42,13 +42,14 @@ const Details = () => {
         <div>
             <Navbar></Navbar>
 
-            <h2 className="text-3xl font-black text-center">{food.name}</h2>
+            <div className="flex items-center justify-evenly m-10">
 
-            <div className="flex items-center">
+                <div className="w-2/5 bg-amber-50 shadow-2xl">
+                    <img className="p-10" src={food.image} alt="" />
+                </div>
 
-                <img className="w-1/2 my-5 mx-5" src={food.image} alt="" />
-
-                <div>
+                <div className="p-10 bg-amber-50">
+                    <h2 className="text-3xl font-bold mb-10">{food.name}</h2>
                     <h3 className="text-xl font-bold">Donar Information</h3>
                     <p><span className="font-semibold">Name:</span> {food.donatorname}</p>
                     <p><span className="font-semibold">Food Pickup Location:</span> {food.location}</p><br /><br />
@@ -58,7 +59,7 @@ const Details = () => {
                     <p><span className="font-semibold">Expired Time(day):</span> {food.expired}</p>
 
                     <div className="my-8">
-                        <button className="btn bg-rose-200 text-rose-700" onClick={() => document.getElementById('my_modal_1').showModal()}>Apply Request</button>
+                        <button className="btn bg-amber-300 text-white rounded-sm" onClick={() => document.getElementById('my_modal_1').showModal()}>Apply Request</button>
                         <form>
                             <dialog id="my_modal_1" className="modal">
                                 <div className="modal-box">
@@ -67,7 +68,7 @@ const Details = () => {
                                     <p className="py-4">Donate (If you want): <input type="text" placeholder="$" className="input input-bordered w-full" /></p>
                                     <div className="modal-action">
                                         <form method="dialog">
-                                            <button onClick={() => handleRequest(food._id, food.donatorname, food.donatoremail, food.donatorimage, food.location, food.expired, food.status, food.image, user.displayName, user.email, user.photoURL)} className="btn bg-rose-200 text-rose-700">Request</button>
+                                            <button onClick={() => handleRequest(food._id, food.donatorname, food.donatoremail, food.donatorimage, food.location, food.expired, food.status, food.image, user.displayName, user.email, user.photoURL)} className="btn bg-amber-300 text-white rounded-sm">Request</button>
                                         </form>
                                     </div>
                                 </div>
