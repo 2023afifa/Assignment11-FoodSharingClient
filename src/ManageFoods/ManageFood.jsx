0ac3a22a7d6db9ca11c7ac4dc5dd25a3b2695982
afifa <2023afifa@gmail.com>
2024-02-21 23:28:52 +0600

    Table updated

--- src/ManageFoods/ManageFood.jsx
+++ src/ManageFoods/ManageFood.jsx
@@ -38,30 +38,32 @@ const ManageFood = ({ c }) => {
     }
 
     return (
-        <div className="w-2/3 mx-auto border-2 ">
-            <tr className="border-2">
-                <td>
-                    <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                                <img src={image} alt="Avatar Tailwind CSS Component" />
+        <div className="w-2/3 mx-auto">
+            <table className="w-full">
+                <tr>
+                    <td className="w-1/3">
+                        <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                                <div className="mask mask-squircle w-12 h-12">
+                                    <img src={image} alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div className="font-bold text-lg">{name}</div>
                             </div>
                         </div>
-                        <div>
-                            <div className="font-bold">{name}</div>
-                        </div>
-                    </div>
-                </td>
-                <th>
-                    <Link to={`/update/${_id}`}><button className="btn btn-ghost btn-xs bg-orange-500 text-white">Edit</button></Link>
-                </th>
-                <th>
-                    <button onClick={() => handleDelete(_id)} className="btn btn-ghost btn-xs bg-red-500 text-white">Delete</button>
-                </th>
-                <th>
-                    <Link to={`/manage/${_id}`}><button className="btn btn-ghost btn-xs bg-amber-500 text-white">Manage</button></Link>
-                </th>
-            </tr>
+                    </td>
+                    <th>
+                        <Link to={`/update/${_id}`}><button className="btn btn-ghost btn-xs bg-orange-500 text-white">Edit</button></Link>
+                    </th>
+                    <th>
+                        <button onClick={() => handleDelete(_id)} className="btn btn-ghost btn-xs bg-red-500 text-white">Delete</button>
+                    </th>
+                    <th>
+                        <Link to={`/manage/${_id}`}><button className="btn btn-ghost btn-xs bg-amber-500 text-white">Manage</button></Link>
+                    </th>
+                </tr>
+            </table>
         </div>
     );
 };
