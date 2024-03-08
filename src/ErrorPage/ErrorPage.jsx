1cf55750e5382e8262a178edab4ff322c88233e3
afifa <2023afifa@gmail.com>
2024-03-08 17:48:51 +0600

    Error page updated

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="my-10">
-            <div className="mb-20 ml-10">
-                <Link to="/"><button className="btn bg-rose-200 text-rose-700">Go to Home</button></Link>
+        <div>
+            <img className="w-2/5 mx-auto" src="https://i.ibb.co/PmQ6J9V/5203172.jpg" alt="" />
+            <div className="text-center">
+                <Link to="/">
+                    <button className="btn bg-transparent normal-case text-amber-500 underline border-none hover:bg-transparent hover:text-amber-600">Go Back To Home Page</button>
+                </Link>
             </div>
-            <img className="w-2/5 mx-auto" src="../../images/error.jpg" alt="" />
         </div>
     );
 };
