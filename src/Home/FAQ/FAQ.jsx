e753424a7b3ce35450b7bc3724a22ee5e3bf9b26
afifa <2023afifa@gmail.com>
2024-03-26 20:00:41 +0600

    FAQ section responsiveness done

--- src/Home/FAQ/FAQ.jsx
+++ src/Home/FAQ/FAQ.jsx
@@ -2,11 +2,11 @@ const FAQ = () => {
     return (
         <div>
             <h2 className="text-center text-2xl font-bold mb-6">How Can We Help You?</h2>
-            <div className="w-4/5 mx-auto mb-10 flex items-center gap-5">
-                <div className="w-1/2">
-                    <img src="https://i.ibb.co/YPD2mh3/FAQ.jpg" alt="" />
+            <div className="lg:w-4/5 mx-10 lg:mx-auto mb-10 lg:flex lg:items-center gap-5">
+                <div className="lg:w-1/2 mb-5">
+                    <img className="rounded" src="https://i.ibb.co/YPD2mh3/FAQ.jpg" alt="" />
                 </div>
-                <div className="w-1/2">
+                <div className="lg:w-1/2">
                     <div className="collapse collapse-plus border-2 border-amber-500 bg-amber-100">
                         <input type="radio" name="my-accordion-1" checked="checked" />
                         <div className="collapse-title text-xl font-medium">
